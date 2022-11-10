1897868cfde8f263f93d36d41ff909c80b2499f1
mericas <alex.mericas@gmail.com>
2022-11-10 08:24:12 -0600

    Update Current based on email from Kingroon

--- configs/2.1.1_TMC2225/Configuration_adv.h
+++ configs/2.1.1_TMC2225/Configuration_adv.h
@@ -2811,7 +2811,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.  May want to change to 850 for all
+    #define X_CURRENT       850        // (mA) RMS current. Multiply by 1.414 for peak current.  Based on max of 1.5A from Kingroon email
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     32        // 0..256
     #define X_RSENSE          0.11
@@ -2831,7 +2831,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       800
+    #define Y_CURRENT       850		// (mA) RMS current. Multiply by 1.414 for peak current.  Based on max of 1.5A from Kingroon email
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     32
     #define Y_RSENSE          0.11
@@ -2851,7 +2851,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       800
+    #define Z_CURRENT       850		// (mA) RMS current. Multiply by 1.414 for peak current.  Based on max of 1.5A from Kingroon email
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     32
     #define Z_RSENSE          0.11
@@ -2951,7 +2951,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      800
+    #define E0_CURRENT      560		// (mA) RMS current. Multiply by 1.414 for peak current.  Based on max of 1A from Kingroon email
     #define E0_MICROSTEPS    32
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
